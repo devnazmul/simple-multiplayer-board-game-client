b71af32f43aeed3c9d7f41517aa76f99021b1e14
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2023-03-27 09:48:57 +0600

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 // api.js
 
-// const API_BASE_URL = "https://simple-socket-io-multi-player-math-game.onrender.com";
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL = "https://simple-socket-io-multi-player-math-game.onrender.com";
+// const API_BASE_URL = "http://localhost:5000";
 
 export async function createGame(
   playerName, 
