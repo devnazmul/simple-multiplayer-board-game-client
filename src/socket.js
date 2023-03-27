024c03e9ddc86b7e323923687662d46de3c6e54c
Md Nazmul Islam <55467254+devnazmul@users.noreply.github.com>
2023-03-27 09:48:22 +0600

    Update socket.js

--- src/socket.js
+++ src/socket.js
@@ -1,7 +1,7 @@
 import io from "socket.io-client";
 
-// const ENDPOINT = "https://simple-socket-io-multi-player-math-game.onrender.com";
-const ENDPOINT = "http://localhost:5000";
+const ENDPOINT = "https://simple-socket-io-multi-player-math-game.onrender.com";
+// const ENDPOINT = "http://localhost:5000";
 // const socket = io(ENDPOINT);
 const socket = io(ENDPOINT, {
   transports: ["websocket"],
